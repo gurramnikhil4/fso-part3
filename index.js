53acca369dfb552e65cfee7982e7c8e5c1be51c6
Gurram Nikhil <gurram.nikhil@outlook.com>
2024-01-31 21:05:20 +0530

    Ex 3.19 Add Validator

--- index.js
+++ index.js
@@ -89,7 +89,9 @@ const errorHandler = (err,req,res,next)=>{
   console.log(err.message)
   if (err.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (err.name === 'ValidationError') {   
+    return res.status(400).json({ error: err.message })
+   }
 
   next(err)
 }
